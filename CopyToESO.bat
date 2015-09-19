a1cd30b98b824f0d6a597d45888375235836e489
tridman <tridman@web.de>
2015-09-19 16:15:49 +0200

    - Updated API-Version to 100012 - Updated LibAddonMenu to 2.0r18

--- CopyToESO.bat
+++ CopyToESO.bat
@@ -1,9 +1,9 @@
 echo off
 
 echo "Removing old directory..."
-rmdir /S /Q "%HOMEDRIVE%%homepath%\Documents\Elder Scrolls Online\liveeu\AddOns\ItemConditionAlert\"
+rmdir /S /Q "F:\home\tridman\Documents\Elder Scrolls Online\liveeu\AddOns\ItemConditionAlert\"
 echo "DONE!"
 
 echo "Coppy new sources...."
-xcopy ItemConditionAlert "%HOMEDRIVE%%homepath%\Documents\Elder Scrolls Online\liveeu\AddOns\ItemConditionAlert" /S /E /C /I
+xcopy ItemConditionAlert "F:\home\tridman\Documents\Elder Scrolls Online\liveeu\AddOns\ItemConditionAlert" /S /E /C /I
 echo "DONE!"
