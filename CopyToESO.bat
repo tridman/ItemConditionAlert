20546690eb75c05ca0835e0f4a2006408b04aad2
tridman <tridman@web.de>
2016-11-23 20:47:59 +0100

    API Update

--- CopyToESO.bat
+++ CopyToESO.bat
@@ -1,9 +1,9 @@
 echo off
 
 echo "Removing old directory..."
-rmdir /S /Q "F:\home\tridman\Documents\Elder Scrolls Online\liveeu\AddOns\ItemConditionAlert\"
+rmdir /S /Q "F:\home\tridman\Documents\Elder Scrolls Online\live\AddOns\ItemConditionAlert\"
 echo "DONE!"
 
 echo "Coppy new sources...."
-xcopy ItemConditionAlert "F:\home\tridman\Documents\Elder Scrolls Online\liveeu\AddOns\ItemConditionAlert" /S /E /C /I
+xcopy ItemConditionAlert "F:\home\tridman\Documents\Elder Scrolls Online\live\AddOns\ItemConditionAlert" /S /E /C /I
 echo "DONE!"
