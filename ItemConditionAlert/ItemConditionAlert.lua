b1d698211733482a7673326a1e6d0860957dd921
tridman <tridman@web.de>
2020-06-13 19:56:50 +0200

    Update to version 1.5.12
    API Version 100031
    Contains LibAddonMenu-2.0r30

--- ItemConditionAlert/ItemConditionAlert.lua
+++ ItemConditionAlert/ItemConditionAlert.lua
@@ -10,7 +10,7 @@ Author:   @tridman
 
 ItemConditionAlert                      = {}
 ItemConditionAlert.Name                 = "ItemConditionAlert"
-ItemConditionAlert.Version              = "1.5.11"
+ItemConditionAlert.Version              = "1.5.12"
 ItemConditionAlert.Command              = "/ica"
 
 ItemConditionAlert.sv                   = {}
